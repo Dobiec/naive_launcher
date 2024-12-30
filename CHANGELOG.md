592255a0fbf829878d9803a0905f32c21041b058
Dobiec <125229275+Dobiec@users.noreply.github.com>
2024-12-30 21:29:32 +0800

    Add webroot

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+#### Changelog v2.1.0
+* Added the script to restart the naive core, which can be executed in KernelSU.
+* Added a web page for restarting the naive core.
+
 #### Changelog v2.0.1
 * Added the update configuration, and it can be updated in KernelSU.
--- action.sh
+++ action.sh
@@ -2,20 +2,34 @@
 
 WORK_DIR=${0%/*}
 # Query all the running naive processes.
-processes=$(ps aux | grep '/data/adb/modules/naive_launcher/bin/naive' | grep -v 'grep' | awk '{print $1}')
+
+# check if ksu
+if ls /data/adb | grep -q "ksu"; then
+    processes=$(/data/adb/ksu/bin/busybox ps aux | grep '/data/adb/modules/naive_launcher/bin/naive' | grep -v 'grep' | awk '{print $1}')
+    /data/adb/ksu/bin/busybox sh /data/adb/modules/naive_launcher/action1.sh
+elif ls /data/adb | grep -q "ap"; then
+    processes=$(/data/adb/ap/bin/busybox ps aux | grep '/data/adb/modules/naive_launcher/bin/naive' | grep -v 'grep' | awk '{print $1}')
+else
+    echo "Error: Neither KernelSU nor Apatch found."
+    exit 1
+fi
+
+echo "Found NaiveProxy Process:"
+echo ${processes}
 
 # Kill naive core
 if [ -n "$processes" ]; then
-    echo "Killing processes: $processes"
     for pid in $processes; do
+        echo "Killing processes: $pid"
         kill -9 $pid
     done
 else
     echo "No naive process found to kill."
 fi
 
+sleep 2s
 # launch naive core
 echo "Starting NaiveProxy..."
 sh ${WORK_DIR}/service.sh
 
-sleep 5s
+sleep 2s
